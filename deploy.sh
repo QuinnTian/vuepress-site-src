8040ea0b239be708691d77bfe044f7471a564eda
QuinnTian <530479731@qq.com>
2022-01-30 18:30:22 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
+npm run build # 生成静态文件
+cd dist # 进入生成的文件夹
 
-# 生成静态文件
-npm run docs:build
-
-# 进入生成的文件夹
-cd dist/
-
-# 如果是发布到自定义域名
-echo 'quinntian.com' > CNAME
-
+# deploy to github
+# echo 'blog.xugaoyi.com' > CNAME
+if [ -z "$AUTO_BUILD" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:QuinnTian/quinntian.github.io.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://QuinnTian:${AUTO_BUILD}@github.com:QuinnTian/quinntian.github.io.git
+  git config --global user.name "QuinnTian"
+  git config --global user.email "530479731@qq.com"
+fi
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
 
-# 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:QuinnTian/QuinnTian.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# deploy to coding
+# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#  codingUrl=git@git.dev.tencent.com:xugaoyi/xugaoyi.git
+# else
+#  codingUrl=https://xugaoyi:${CODING_TOKEN}@git.dev.tencent.com/xugaoyi/xugaoyi.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
 
 cd -
+rm -rf dist
